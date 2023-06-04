259318d04fbb50f5a92652ee733002ba76cee046
nordstromkristofer <nordstromkristofer@gmail.com>
2023-06-04 23:03:34 +0200

    controller

---
+++ backoffice/appusAPI/models/vacationModel.cs
@@ -0,0 +1,11 @@
+public class VacationItem
+{
+  public int Id { get; set; }
+  public string DateOfChange { get; set; }
+  public string StartDate { get; set; }
+  public string EndDate { get; set; }
+  public string Member { get; set; }
+  public int Approved { get; set; }
+  public int Type { get; set; }
+  public string Comment { get; set; }
+}
