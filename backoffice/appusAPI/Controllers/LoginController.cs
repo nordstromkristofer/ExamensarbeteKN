81c3196aab4f2b5d298b05e40662581e6c69ad6c
nordstromkristofer <nordstromkristofer@gmail.com>
2023-06-05 11:59:19 +0200

    JWT imp

--- backoffice/appusAPI/Controllers/LoginController.cs
+++ backoffice/appusAPI/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -10,38 +11,44 @@ namespace YourNamespace.Controllers
   // ...
 
   [AllowAnonymous]
-  [HttpPost("login")]
-  public IActionResult Login(LoginRequest request)
+  [Route("api/[controller]")]
+  [ApiController]
+  public class LoginController : ControllerBase
   {
-    // Dummy username and password
-    string dummyUsername = "dummyuser";
-    string dummyPassword = "dummypassword";
-
-    if (request.Username == dummyUsername && request.Password == dummyPassword)
+    [HttpPost]
+    [Route("login")]
+    public IActionResult Login(LoginRequest request)
     {
-      var token = GenerateJwtToken(dummyUsername);
-      return Ok(new { Token = token });
+      // Dummy username and password
+      string dummyUsername = "dummyuser";
+      string dummyPassword = "dummypassword";
+
+      if (request.Username == dummyUsername && request.Password == dummyPassword)
+      {
+        var token = GenerateJwtToken(dummyUsername);
+        return Ok(new { Token = token });
+      }
+
+      return Unauthorized();
     }
 
-    return Unauthorized();
-  }
-
-  private string GenerateJwtToken(string username)
-  {
-    var tokenHandler = new JwtSecurityTokenHandler();
-    var key = Encoding.UTF8.GetBytes("your-super-secret-key");
-    var tokenDescriptor = new SecurityTokenDescriptor
+    private string GenerateJwtToken(string username)
     {
-      Subject = new ClaimsIdentity(new[]
-        {
-                new Claim(ClaimTypes.Name, username)
-            }),
-      Expires = DateTime.UtcNow.AddDays(7),
-      SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-    };
-    var token = tokenHandler.CreateToken(tokenDescriptor);
-    return tokenHandler.WriteToken(token);
-  }
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var key = Encoding.UTF8.GetBytes("your-super-secret-key"); // Replace with your secure secret key
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(new[]
+          {
+                    new Claim(ClaimTypes.Name, username)
+                }),
+        Expires = DateTime.UtcNow.AddDays(7),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+      };
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      return tokenHandler.WriteToken(token);
+    }
 
-  // ...
+    // ...
+  }
 }
